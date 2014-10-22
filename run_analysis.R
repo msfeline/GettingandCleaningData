b3251f2d8f6656891414a21e0f4e38b1eb0ab125
Kelly <msfeline@gmail.com>
2014-10-22 15:18:31 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -21,21 +21,19 @@ dim(joinLabel) # 10299*1
 joinSubject <- rbind(trainSubject, testSubject)
 dim(joinSubject) # 10299*1
 
-# Step2. Extracts only the measurements on the mean and standard 
-# deviation for each measurement. 
+# Step2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 features <- read.table("./UCI HAR Dataset/features.txt")
 dim(features)  # 561*2
 meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
 length(meanStdIndices) # 66
 joinData <- joinData[, meanStdIndices]
 dim(joinData) # 10299*66
-names(joinData) <- gsub("\\(\\)", "", features[meanStdIndices, 2]) # remove "()"
-names(joinData) <- gsub("mean", "Mean", names(joinData)) # capitalize M
-names(joinData) <- gsub("std", "Std", names(joinData)) # capitalize S
-names(joinData) <- gsub("-", "", names(joinData)) # remove "-" in column names 
+names(joinData) <- gsub("\\(\\)", "", features[meanStdIndices, 2]) 
+names(joinData) <- gsub("mean", "Mean", names(joinData)) 
+names(joinData) <- gsub("std", "Std", names(joinData)) 
+names(joinData) <- gsub("-", "", names(joinData)) 
 
-# Step3. Uses descriptive activity names to name the activities in 
-# the data set
+# Step3. Uses descriptive activity names to name the activities in the data set
 activity <- read.table("./UCI HAR Dataset/activity_labels.txt")
 activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
 substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
@@ -44,17 +42,15 @@ activityLabel <- activity[joinLabel[, 1], 2]
 joinLabel[, 1] <- activityLabel
 names(joinLabel) <- "activity"
 
-# Step4. Appropriately labels the data set with descriptive activity 
-# names. 
+# Step4. Appropriately labels the data set with descriptive activity names. 
 names(joinSubject) <- "subject"
 cleanedData <- cbind(joinSubject, joinLabel, joinData)
 dim(cleanedData) # 10299*68
-write.table(cleanedData, "merged_data.txt") # write out the 1st dataset
+write.table(cleanedData, "merged_data.txt") # this part will write out the 1st dataset
 
-# Step5. Creates a second, independent tidy data set with the average of 
-# each variable for each activity and each subject. 
-subjectLen <- length(table(joinSubject)) # 30
-activityLen <- dim(activity)[1] # 6
+# Step5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+subjectLen <- length(table(joinSubject))
+activityLen <- dim(activity)[1] 
 columnLen <- dim(cleanedData)[2]
 result <- matrix(NA, nrow=subjectLen*activityLen, ncol=columnLen) 
 result <- as.data.frame(result)
@@ -71,7 +67,7 @@ for(i in 1:subjectLen) {
     }
 }
 head(result)
-write.table(result, "data_with_means.txt") # write out the 2nd dataset
+write.table(result, "data_with_means.txt") # this part will write out the 2nd dataset
 
 
 
